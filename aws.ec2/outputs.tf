46c8d2c0ea6ab8aabc32e947c4f406b292a01c37
Kundan Kumar <kundan@Kundans-MacBook-Air.local>
2025-07-11 02:54:17 +0530

    3rd day

--- aws.ec2/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "aws_instance_public_ip" {
-    value = aws_instance.myserver.public_ip
-    description = "The public IP address of the EC2 instance"
-}
--- aws.ec2/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "region" {
-  description = "The AWS region to deploy the resources in"
-  type        = string
-  default     = "eu-north-1"
-}
